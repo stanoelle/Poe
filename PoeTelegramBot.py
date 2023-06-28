9f41f454d1f7fbb2038dfb85ace874d373e6270b
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-06-28 08:27:16 +0530

    Update PoeTelegramBot.py

--- PoeTelegramBot.py
+++ PoeTelegramBot.py
@@ -22,8 +22,8 @@ from telegram.ext import (
 load_dotenv()
 
 # Get environment variables
-TELEGRAM_TOKEN = os.getenv("BOT_TOKEN")
-POE_COOKIE = os.getenv("POE_COOKIE")
+TELEGRAM_TOKEN = "6179975944:AAEgrJwmzF0urBQOMYOVhGyosAFGoGYTc14"
+POE_COOKIE = "m87UlQ4NDefo_CAwj-9kCQ%3D%3D"
 ALLOWED_USERS = os.getenv("ALLOWED_USERS")
 ALLOWED_CHATS = os.getenv("ALLOWED_CHATS")
 
